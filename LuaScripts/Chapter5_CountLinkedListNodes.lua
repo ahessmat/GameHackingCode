6c21d8eb75cf6b93825b5305986e946da0ff2c86
Asa <ahessmat@gmail.com>
2025-05-02 17:02:21 -0700

    Updated binary list; initial commit

--- LuaScripts/Chapter5_CountLinkedListNodes.lua
+++
@@ -1,9 +0,0 @@
-function countLinkedListNodes(nodeAddress)
-    local counter = 0
-    local next = readInteger(nodeAddress)
-    while (next ~= nodeAddress) do
-        counter = counter + 1
-        next = readInteger(next)
-    end
-    return counter
-end
